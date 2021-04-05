bb5aa7b12c5d4284ec429c150ef4354bfb464ef2
absaki <28054064+absaki@users.noreply.github.com>
2021-04-06 02:23:50 +0900

    [add] add metallb

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ docker build -t fts-nginx ./srcs/nginx/
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f ./srcs/metallb/configmap.yaml
 
 ## Start cluster
+kubectl apply -f ./srcs/nginx/deployment.yaml
 kubectl apply -f ./srcs/appcluster.yaml
