b3f4e449dec7970397f9ec64b46bd4c25dde1184
absaki <28054064+absaki@users.noreply.github.com>
2021-04-06 14:32:24 +0900

    [fix] combine manifests

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 cd `dirname $0`
 read -p "Enter memory size for minikube. (like 2048m or 8g) > " memsize
 let processors=`grep processor /proc/cpuinfo | wc -l`
-minikube start --cpus=$((processors-1)) --memory=$memsize
+minikube start --driver=hdocker --cpus=$((processors-1)) --memory=$memsize
 minikube dashboard &> /dev/null &
 
 # Container image preparation
@@ -16,8 +16,6 @@ docker build -t fts-nginx ./srcs/nginx/
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f ./srcs/metallb/configmap.yaml
 
 ## Start cluster
-kubectl apply -f ./srcs/nginx/deployment.yaml
 kubectl apply -f ./srcs/appcluster.yaml
