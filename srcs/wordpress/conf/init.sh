70be4023eac15cef99644be5ead8b46d5b5ad96e
absaki <28054064+absaki@users.noreply.github.com>
2021-04-07 23:45:14 +0900

    [add] php-fpm in wordpress pod

--- srcs/wordpress/conf/init.sh
+++ srcs/wordpress/conf/init.sh
@@ -1,5 +1,11 @@
 #!bin/ash
 
+# PHP-FPM
+rm /etc/php7/php-fpm.d/www.conf
+mv /tmp/www.conf /etc/php7/php-fpm.d/www.conf
+php-fpm7
+rc-service php-fpm7 start
+
 # NGINX
 adduser -D -g 'www' www
 mkdir -p /run/nginx
@@ -8,6 +14,7 @@ chown -R www:www /var/www
 mv /etc/nginx/http.d/default.conf /etc/nginx/http.d/default.conf.original
 cp /tmp/nginx_top.conf /etc/nginx/http.d/
 cp /tmp/index.html /var/www/html/index.html
+cp /tmp/phpinfo.php /var/www/html/phpinfo.php
 
 # START SERVER
 nginx -g 'daemon off;'
