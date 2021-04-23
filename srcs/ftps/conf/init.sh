4bd274fac4a1fd30216ea0899054ecf575970a97
absaki <28054064+absaki@users.noreply.github.com>
2021-04-23 13:57:42 +0900

    [fix] ftps permissions

--- srcs/ftps/conf/init.sh
+++ srcs/ftps/conf/init.sh
@@ -4,4 +4,5 @@ adduser --home=/etc/ftps/wpadmin -D wpadmin
 echo "wpadmin:wpadmin" | chpasswd
 echo "wpadmin" > /etc/vsftpd/vsftpd.userlist
 telegraf &
+chown -R wpadmin /etc/ftps/wpadmin
 vsftpd /etc/vsftpd/vsftpd.conf
