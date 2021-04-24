d1638c267b522630bd7557834607d5794a1a92d4
absaki <28054064+absaki@users.noreply.github.com>
2021-04-24 18:41:06 +0900

    [add] navigation page

--- srcs/nginx/conf/init.sh
+++ srcs/nginx/conf/init.sh
@@ -10,11 +10,12 @@
 adduser -D -g 'www' www
 mkdir -p /run/nginx
 mkdir -p /var/www/html
-chown -R www:www /var/www
 mv /etc/nginx/http.d/default.conf /etc/nginx/http.d/default.conf.original
 cp /tmp/nginx_top.conf /etc/nginx/http.d/
-cp /tmp/index.html /var/www/html/index.html
-
+rmdir /var/www/html
+mv /tmp/public /var/www/html
+chmod -R 755 /var/www/html
+chown -R www:www /var/www
 # TELEGRAF
 telegraf &
 
