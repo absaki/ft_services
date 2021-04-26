821d43a73961f03a34d419703ac436f09d0536b3
absaki <28054064+absaki@users.noreply.github.com>
2021-04-26 14:05:05 +0900

    [fix] ftps upload bug

--- srcs/ftps/conf/init.sh
+++ srcs/ftps/conf/init.sh
@@ -4,5 +4,5 @@ adduser --home=/etc/ftps/wpadmin -D wpadmin
 echo "wpadmin:wpadmin" | chpasswd
 echo "wpadmin" > /etc/vsftpd/vsftpd.userlist
 telegraf &
-chown -R wpadmin /etc/ftps/wpadmin
+chmod -R 777 /etc/ftps/wpadmin
 vsftpd /etc/vsftpd/vsftpd.conf
